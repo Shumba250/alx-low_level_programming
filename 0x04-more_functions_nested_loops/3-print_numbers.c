02b5341a191e64fb6cbc1b747a2f73a4fcb7f97f
Shumba250 <mudakikwaaimable05@gmail.com>
2022-04-14 07:37:07 -0700

    printng numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - print numbers from 0-9
+*Return: Always 0 (sucess)
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
