3a7e0b409c241a0601ec3c1be60770f9668f599a
Shumba250 <mudakikwaaimable05@gmail.com>
2022-04-20 11:47:29 -0700

    function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+int _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -8,5 +10,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 
 void _puts(char *str);
+
+void print_rev(char *s);
 
 #endif
