82d07eb0aff81ea82a590291100f0392a65920f7
Shumba250 <mudakikwaaimable05@gmail.com>
2022-04-24 09:41:24 -0700

    a function that prints a buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@ int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
+void print_buffer(char *b, int size);
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_number(int n);
